25caeea921d88e143fd95a019a82e1fa3cdb592b
Gonyb <344097091@qq.com>
2019-11-18 19:38:46 +0800

    第二章前3小节完成

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_01/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_01/FunctionExamples.java
@@ -20,6 +20,12 @@ public class FunctionExamples {
 
   public static final Function<Integer, Integer> compose(final Function<Integer, Integer> f1,
                                                          final Function<Integer, Integer> f2) {
-    throw new RuntimeException("To be implemented.");
+    return new Function<Integer, Integer>(){
+
+      @Override
+      public Integer apply(Integer arg) {
+        return f1.apply(f2.apply(arg));
+      }
+    };
   }
 }
