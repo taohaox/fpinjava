d4a5678eadc0832ec32cd67d0fabf4a11f8021de
Gonyb <344097091@qq.com>
2019-11-21 00:22:30 +0800

    3.1章完成

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_01/EmailValidation.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_01/EmailValidation.java
@@ -1,29 +1,32 @@
 package com.fpinjava.makingjavafunctional.exercise03_01;
 
 
-import java.util.regex.Pattern;
-
 import com.fpinjava.common.Function;
-import com.fpinjava.makingjavafunctional.exercise03_01.Effect;
-import com.fpinjava.makingjavafunctional.exercise03_01.Result;
+
+import java.util.regex.Pattern;
 
 public class EmailValidation {
 
-  static Pattern emailPattern =
-      Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
+    static Pattern emailPattern =
+            Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-  static Function<String, Result<String>> emailChecker = s -> {
-    throw new RuntimeException("To be implemented");
-  };
+    static Function<String, Result<String>> emailChecker = s -> {
+        if (s == null) {
+            return Result.failure("email must not be null");
+        } else if ("".equals(s)) {
+            return Result.failure("email must not be empty");
+        }
+        return emailPattern.matcher(s).matches()?Result.success(s):Result.failure("email "+s+" is invalid");
+    };
 
-  public static void main(String... args) {
-    emailChecker.apply("this.is@my.email").bind(success, failure);
-    emailChecker.apply(null).bind(success, failure);
-    emailChecker.apply("").bind(success, failure);
-    emailChecker.apply("john.doe@acme.com").bind(success, failure);
-  }
+    public static void main(String... args) {
+        emailChecker.apply("this.is@my.email").bind(success, failure);
+        emailChecker.apply(null).bind(success, failure);
+        emailChecker.apply("").bind(success, failure);
+        emailChecker.apply("john.doe@acme.com").bind(success, failure);
+    }
 
-  static Effect<String> success = null; // To be implemented
-  
-  static Effect<String> failure = null; // To be implemented
+    static Effect<String> success = s-> System.out.println("mail send to "+s);
+
+    static Effect<String> failure = s-> System.err.println("error message logged:"+s); // To be implemented
 }
--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_01/Result.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_01/Result.java
@@ -2,42 +2,42 @@ package com.fpinjava.makingjavafunctional.exercise03_01;
 
 public interface Result<T> {
 
-  void bind(Effect<T> success, Effect<String> failure);
+    void bind(Effect<T> success, Effect<String> failure);
 
-  static <T> Result<T> failure(String message) {
-    return new Failure<>(message);
-  }
-
-  static <T> Result<T> success(T value) {
-    return new Success<>(value);
-  }
-
-  class Success<T> implements Result<T> {
-
-    private final T value;
-
-    private Success(T t) {
-      value = t;
+    static <T> Result<T> failure(String message) {
+        return new Failure<>(message);
     }
 
-    @Override
-    public void bind(Effect<T> success, Effect<String> failure) {
-      throw new RuntimeException("To be implemented.");
-    }
-  }
-
-  class Failure<T> implements Result<T> {
-
-    private final String errorMessage;
-
-    private Failure(String s) {
-      this.errorMessage = s;
+    static <T> Result<T> success(T value) {
+        return new Success<>(value);
     }
 
-    @Override
-    public void bind(Effect<T> success, Effect<String> failure) {
-      throw new RuntimeException("To be implemented.");
+    class Success<T> implements Result<T> {
+
+        private final T value;
+
+        private Success(T t) {
+            value = t;
+        }
+
+        @Override
+        public void bind(Effect<T> success, Effect<String> failure) {
+            success.apply(value);
+        }
     }
-  }
+
+    class Failure<T> implements Result<T> {
+
+        private final String errorMessage;
+
+        private Failure(String s) {
+            this.errorMessage = s;
+        }
+
+        @Override
+        public void bind(Effect<T> success, Effect<String> failure) {
+            failure.apply(errorMessage);
+        }
+    }
 
 }
