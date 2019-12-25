5dd3fabca555bcac9bbb180064bf801c9e404989
Gonyb <344097091@qq.com>
2019-12-25 15:35:12 +0800

    doc转换pdf  采用自定义类加载器  防止项目中jar包冲突

--- convert-pdf/src/main/java/com/gonyb/util/Main.java
+++ convert-pdf/src/main/java/com/gonyb/util/Main.java
@@ -1,6 +1,6 @@
 package com.gonyb.util;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
@@ -10,6 +10,15 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         ConvertPdfUtil convertPdfUtil = new ConvertPdfUtil();
-        convertPdfUtil.accept(new FileInputStream("d://test.docx"),"d://test.pdf");
+//        System.setProperty("fontPath",Main.class.getClassLoader().getResource("font/simfang.ttf").getPath());
+//        convertPdfUtil.accept(new FileInputStream("C:\\Users\\admin-002\\Desktop\\work\\海豚网FMS功能数据权限隔离方案.docx"),
+//                "d://test1.pdf");
+        new Main().getPathPrintln();
+    }
+
+    public void getPathPrintln(){
+        System.out.println(this.getClass().getClassLoader().getResource(""));
+        System.out.println(new File(this.getClass().getResource("/").getFile()).getParent());
+        System.out.println(System.getProperty("user.dir"));
     }
 }
